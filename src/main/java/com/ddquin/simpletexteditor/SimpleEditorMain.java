3dc951d9544f8f7792648a245cfc5b6b5e63ac06
DDQuin <ddquin420@gmail.com>
2022-05-01 22:14:35 +0100

    Hint text goes away after a few seconds, Updatebottom bar gets called when text changes, if not saved will show on botttom

--- src/main/java/com/ddquin/simpletexteditor/SimpleEditorMain.java
+++ src/main/java/com/ddquin/simpletexteditor/SimpleEditorMain.java
@@ -1,23 +1,13 @@
 package com.ddquin.simpletexteditor;
 
-import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyCharacterCombination;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -25,16 +15,26 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SimpleEditorMain {
 
     private File fileOpen;
 
+    @FXML
+    private MenuBar menuBar;
+
     private double screenWidth;
     private double screenHeight;
+    private boolean isSaved;
+
+
 
     @FXML
     private TextArea textArea;
+
+    // Bottom Bar
 
     @FXML
     private Text lineCount;
@@ -45,12 +45,13 @@ public class SimpleEditorMain {
     @FXML
     private Text fileText;
 
-    @FXML
-    private MenuBar menuBar;
+    private Timer timeToCloseHint;
+
 
 
     @FXML
     public void initialize() {
+        timeToCloseHint = new Timer();
         setUpMenus();
         setUpTextArea();
     }
@@ -60,6 +61,10 @@ public class SimpleEditorMain {
     }
 
     private void setUpTextArea() {
+        textArea.textProperty().addListener((observable, oldVal, newVal) -> {
+            isSaved = false;
+            updateBottomBar();
+        });
         textArea.addEventFilter(KeyEvent.KEY_PRESSED, keyEvent -> {
             KeyCombination increaseCombo = new KeyCharacterCombination("=", KeyCombination.SHORTCUT_DOWN);
             KeyCombination decreaseCombo = new KeyCharacterCombination("-", KeyCombination.SHORTCUT_DOWN);
@@ -69,9 +74,7 @@ public class SimpleEditorMain {
                 changeFontSize(textArea.getFont().getSize() - 2);
             }
         });
-        textArea.setOnKeyTyped( event -> updateLineCount());
-        textArea.setOnMouseClicked( event -> updateLineCount());
-        updateLineCount();
+        updateBottomBar();
     }
 
 
@@ -85,10 +88,11 @@ public class SimpleEditorMain {
         String content;
         try {
             content = Files.readString(fileOpen.toPath(), StandardCharsets.US_ASCII);
-            hintText.setText("Opened " + fileOpen.getName());
-            changeTitle(fileOpen.getName());
+            showHintText("Opened" + fileOpen.getName());
+            setFileOpenText(fileOpen.getName());
             textArea.setText(content);
-            updateLineCount();
+            isSaved = true;
+            updateBottomBar();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -98,12 +102,14 @@ public class SimpleEditorMain {
     @FXML
     public void saveFile() {
         if (fileOpen == null) {
-            hintText.setText("No file open!");
+            showHintText("No file open!");
             return;
         }
         try (PrintWriter out = new PrintWriter(fileOpen)) {
             out.println(textArea.getText());
-            hintText.setText("Saved " + fileOpen.getName());
+            showHintText("Saved " + fileOpen.getName());
+            isSaved = true;
+            updateBottomBar();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
@@ -112,21 +118,26 @@ public class SimpleEditorMain {
     @FXML
     public void closeFile() {
         if (fileOpen == null) {
-            hintText.setText("No file open!");
+            showHintText("No file open!");
             return;
         }
-        hintText.setText("Closed " + fileOpen.getName());
+        showHintText("Closed " + fileOpen.getName());
         fileOpen = null;
-        changeTitle("");
+        setFileOpenText("");
         textArea.setText("");
-        updateLineCount();
     }
 
-    private void updateLineCount() {
+    private void updateBottomBar() {
         lineCount.setText(" Lines: " + textArea.getText().lines().count());
+        if (fileOpen != null && !isSaved) {
+            setFileOpenText(fileOpen.getName() + " *");
+        }
+        if (fileOpen != null && isSaved) {
+            setFileOpenText(fileOpen.getName());
+        }
     }
 
-    private void changeTitle(String newTitle) {
+    private void setFileOpenText(String newTitle) {
         fileText.setText(newTitle);
     }
 
@@ -137,15 +148,17 @@ public class SimpleEditorMain {
     }
 
     private void showHintText(String text) {
+        menuBar.getScene().getWindow().setOnCloseRequest(e -> {
+            if (timeToCloseHint != null) timeToCloseHint.cancel();
+        });
         hintText.setText(text);
+        TimerTask closeHint = new TimerTask() {
+            @Override
+            public void run() {
+                hintText.setText("");
+            }
+        };
+        timeToCloseHint.schedule(closeHint, 3000);
     }
 
-
-    public double getScreenWidth() {
-        return screenWidth;
-    }
-
-    public double getScreenHeight() {
-        return screenHeight;
-    }
 }
